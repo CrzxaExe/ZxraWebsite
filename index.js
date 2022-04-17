f88745296c8bbe1951f4af018cb7b395977f74b8
CrzxaExe <craserchannel@outlook.co.id>
2022-04-17 10:02:30 +0700

    Update

---
+++ index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const config = require('./config.json');
+const app = express();
+const port = 3000;
+
+mongoose.connect(config.mongo).then((connected) => {
+  console.log("Connected")
+}), { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true };
+
+app.get('/', (req,res) => {
+  res.sendFile('./index.html', {root: __dirname})
+});
+
+app.listen(port, () => {
+  console.log("Setting Website")
+})
